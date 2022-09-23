48270b2283db9b8ac2b296cc6db9225d10428f65
dev-lotus <lotusbiswas2022@gmail.com>
2022-09-24 00:34:52 +0530

    CRUD Operation basic

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost:27017/AlienDBex'
+
+// Initliaze express server 
+const app = express()
+
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected');
+})
+
+app.use(express.json())
+
+// Router 
+const userRouter = require('./routes/user')
+// Middleware
+app.use('/user', userRouter)
+
+app.listen(9000, () => {
+    console.log('Server started')
+})
