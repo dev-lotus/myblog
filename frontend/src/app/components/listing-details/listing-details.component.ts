651b2d03eb5b853f50927c8f1baa4d4477cc78c7
dev-lotus <102558895+dev-lotus@users.noreply.github.com>
2022-10-02 04:36:55 +0530

    Like logic fix on one time click
    double time click to unlike to be implemented later

--- frontend/src/app/components/listing-details/listing-details.component.ts
+++ frontend/src/app/components/listing-details/listing-details.component.ts
@@ -9,6 +9,7 @@ import { FreeListing } from 'src/app/interface/freeListing';
 import { User } from 'src/app/interface/user';
 import { FreeListServiceService } from 'src/app/services/freeList-service/free-list-service.service';
 import { UserServiceService } from 'src/app/services/user-service/user-service.service';
+import { count } from 'rxjs';
 @Component({
   selector: 'app-listing-details',
   templateUrl: './listing-details.component.html',
@@ -23,7 +24,7 @@ export class ListingDetailsComponent implements OnInit {
   // data
   source: any;
   markers: any;
-  
+
   freeListing: FreeListing[] = [
     {
       "_id": "",
@@ -67,18 +68,20 @@ export class ListingDetailsComponent implements OnInit {
   errMsg!: string;
   status!: any;
   freeListId: string;
-  freeListUserToken!: string ;
-  constructor(private route: ActivatedRoute,private spinner: NgxSpinnerService, private _toast: NgToastService, private _router: Router, private _userService: UserServiceService, private _freeList: FreeListServiceService) {
-       (mapboxgl as any).accessToken = environment.mapbox.accessToken ;
-       this.userToken = String(localStorage.getItem("userId"));
-       const routeParams = this.route.snapshot.paramMap;
-       this.freeListId = String(routeParams.get('id'));
+  freeListUserToken!: string;
+  likesListing:boolean = false;
+  countLikesListing:any[] =[];
+  constructor(private route: ActivatedRoute, private spinner: NgxSpinnerService, private _toast: NgToastService, private _router: Router, private _userService: UserServiceService, private _freeList: FreeListServiceService) {
+    (mapboxgl as any).accessToken = environment.mapbox.accessToken;
+    this.userToken = String(localStorage.getItem("userId"));
+    const routeParams = this.route.snapshot.paramMap;
+    this.freeListId = String(routeParams.get('id'));
 
-       const urlParams = new URLSearchParams(window.location.search);
-       this.freeListUserToken  = String(urlParams.get('token'));
+    const urlParams = new URLSearchParams(window.location.search);
+    this.freeListUserToken = String(urlParams.get('token'));
 
-       console.log(this.freeListUserToken);
-   
+    console.log(this.freeListUserToken);
+
   }
 
   ngOnInit(): void {
@@ -108,58 +111,178 @@ export class ListingDetailsComponent implements OnInit {
       zoom: 14,
       center: [lng, lat]
     });
-    
 
-   const trackPin = 
+
+    const trackPin =
       new mapboxgl.GeolocateControl({
-      positionOptions: {
-      enableHighAccuracy: true
-      },
-      // When active the map will receive updates to the device's location as it changes.
-      trackUserLocation: true,
-      // Draw an arrow next to the location dot to indicate which direction the device is heading.
-      showUserHeading: true
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
       });
 
-   
+
+  }
+  getFreeListUserData() {
+    this.spinner.show();
+    this._freeList.getFreeListingDataById(this.freeListId, this.freeListUserToken).subscribe(
+      res => {
+        setTimeout(() => {
+          /** spinner ends after 5 seconds */
+          this.spinner.hide();
+        }, 1000);
+        this.freeListing = res;
+        console.log(this.freeListing);
+        for (var i = 0; i < this.freeListing.length; i++) {
+          this._userService.getUserDataById(this.freeListUserToken).subscribe(
+            res => {
+              setTimeout(() => {
+                /** spinner ends after 5 seconds */
+                this.spinner.hide();
+              }, 1000);
+
+              this.user.push(res[0]);
+
+              console.log(this.user[1]);
+              // console.log(this.user);
+
+              console.log(this.removeDupliactes(this.user));
+              this.lng = this.freeListing[0].location.lng;
+              this.lat = this.freeListing[0].location.lat;
+              console.log(this.lng, this.lat);
+              this.setHomeLocation(this.lng, this.lat);
+              console.log(this.removeDupliactes(this.freeListing[0].likes));
+              this.countLikesListing = this.removeDupliactes(this.freeListing[0].likes);
+              console.log(this.countLikesListing);
+              if (this.freeListing[0].likes.length == 1) {
+                if (this.freeListing[0].likes[0].listId == this.freeListId && this.freeListing[0].likes[0].userToken == this.userToken) {
+                  this.likesListing = true;
+                  console.log(this.likesListing);
+                }
+                else {
+                  this.likesListing = false;
+
+                }
+              }
+              else {
+                for (var k = 0; k < this.freeListing[0].likes.length; k++) {
+                  if (this.freeListing[0].likes[i].listId == this.freeListId && this.freeListing[0].likes[i].userToken == this.userToken) {
+                    this.likesListing = true;
+                    console.log(this.likesListing);
+                  }
+                  else {
+                    this.likesListing = false;
+
+                  }
+                }
+
+              }
+            }, err => {
+              setTimeout(() => {
+                /** spinner ends after 5 seconds */
+                this.spinner.hide();
+              }, 1000);
+              this.user = [];
+              this.errMsg = err;
+              console.log(this.errMsg)
+            }, () => console.log("Get User Data method excuted successfully"))
+
+        }
+      }, err => {
+        setTimeout(() => {
+          /** spinner ends after 5 seconds */
+          this.spinner.hide();
+        }, 1000);
+        this.freeListing = [];
+        this.errMsg = err;
+        console.log(this.errMsg)
+      }, () => console.log("Get ALL FREE LIST Method excuted successfully"));
+
+    console.log("Free List " + this.freeListing[0]);
+
+    console.log("User " + this.user);
+  }
+  removeDupliactes = (values: any[]) => {
+    let concatArray = values.map(eachValue => {
+      return Object.values(eachValue).join('')
+    })
+    let filterValues = values.filter((value, index) => {
+      return concatArray.indexOf(concatArray[index]) === index
+
+    })
+    return filterValues;
+  }
+
+  setHomeLocation(longitude: number, latitude: number) {
+
+    this.buildMap(latitude, longitude);
+
+    const homeLocationJson = {
+      type: 'Listing Location',
+      features: [
+        {
+          type: 'Listing Location',
+          geometry: {
+            type: 'Point',
+            'coordinates': {
+              'lng': longitude,
+              'lat': latitude
+            }
+          },
+          properties: {
+            title: 'Home'
+          }
+        }
+      ]
+    };
+
+    for (const feature of homeLocationJson.features) {
+      const el = document.createElement('div');
+      el.className = 'marker';
+      el.style.backgroundImage = "url(./../../../assets/images/icons/freeListing.png)";
+
+      new mapboxgl.Marker(el)
+        .setLngLat([feature.geometry.coordinates.lng, feature.geometry.coordinates.lat])
+        .addTo(this.map);
+
+    }
+
+  }
+
+  addLikeFreeListItem(listId: string, userTokenVal: string) {
+    var count =0;
+
+    for(var i=0;i<this.freeListing[0].likes.length;i++)
+    {
+     
+      if(this.freeListing[0].likes[i].listId == listId && this.freeListing[0].likes[i].userToken)
+      {
+        count++;
       }
-      getFreeListUserData() {
-        this.spinner.show();
-        this._freeList.getFreeListingDataById(this.freeListId, this.freeListUserToken).subscribe(
+      
+    }
+    if(count==0)
+      {
+        this._freeList.updateAddLikeFreeListing(listId, userTokenVal).subscribe(
           res => {
             setTimeout(() => {
               /** spinner ends after 5 seconds */
               this.spinner.hide();
             }, 1000);
-            this.freeListing = res;
-            console.log(this.freeListing);
-            for (var i = 0; i < this.freeListing.length; i++) {
-              this._userService.getUserDataById(this.freeListUserToken).subscribe(
-                res => {
-                  setTimeout(() => {
-                    /** spinner ends after 5 seconds */
-                    this.spinner.hide();
-                  }, 1000);
+            this.status = res;
+            console.log(this.status);
+            if (this.status == true) {
+              this._toast.success({ detail: "SUCCESS", summary: 'You liked this listing', position: 'br' });
+              setTimeout(function () {
+                window.location.reload();
+              }, 2000);
     
-                  this.user.push(res[0]);
-    
-                  console.log(this.user[1]);
-                  // console.log(this.user);
-    
-                  console.log(this.removeDupliactes(this.user) );
-                  this.lng = this.freeListing[0].location.lng;
-                  this.lat =  this.freeListing[0].location.lat;
-                  console.log(this.lng, this.lat);
-                  this.setHomeLocation(this.lng,this.lat);
-                }, err => {
-                  setTimeout(() => {
-                    /** spinner ends after 5 seconds */
-                    this.spinner.hide();
-                  }, 1000);
-                  this.user = [];
-                  this.errMsg = err;
-                  console.log(this.errMsg)
-                }, () => console.log("Get User Data method excuted successfully"))
+            }
+            else {
+              this._toast.warning({ detail: "FAILED", summary: 'Unable to like this listing', position: 'br' });
     
             }
           }, err => {
@@ -167,95 +290,18 @@ export class ListingDetailsComponent implements OnInit {
               /** spinner ends after 5 seconds */
               this.spinner.hide();
             }, 1000);
-            this.freeListing = [];
             this.errMsg = err;
-            console.log(this.errMsg)
-          }, () => console.log("Get ALL FREE LIST Method excuted successfully"));
+            this._toast.warning({ detail: "FAILED", summary: 'Please try after sometime', position: 'br' });
     
-        console.log("Free List " + this.freeListing[0]);
-    
-        console.log("User " + this.user);
+          },
+          () => console.log("LIKE LISTING FREE successfully EXECUTED")
+        )
       }
-      removeDupliactes = (values: any[]) => {
-        let concatArray = values.map(eachValue => {
-          return Object.values(eachValue).join('')
-        })
-        let filterValues = values.filter((value, index) => {
-          return concatArray.indexOf(concatArray[index]) === index
-      
-        })
-        return filterValues
+      else{
+        alert("You already Liked it");
       }
-
-      setHomeLocation(longitude:number, latitude: number) {
-
-        this.buildMap(latitude,longitude);
-
-        const homeLocationJson = {
-          type: 'Listing Location',
-          features: [
-            {
-              type: 'Listing Location',
-              geometry: {
-                type: 'Point',
-                'coordinates': {
-                  'lng': longitude,
-                  'lat': latitude
-                }
-              },
-              properties: {
-                title: 'Home'
-              }
-            }
-          ]
-        };
-      
-        for (const feature of homeLocationJson.features) {
-          const el = document.createElement('div');
-          el.className = 'marker';
-          el.style.backgroundImage = "url(./../../../assets/images/icons/freeListing.png)";
-      
-          new mapboxgl.Marker(el)
-            .setLngLat([feature.geometry.coordinates.lng, feature.geometry.coordinates.lat])
-            .addTo(this.map);
-      
-        }
-        
-      }
-
-      addLikeFreeListItem(listId:string, userTokenVal:string)
-  {
     console.log(listId, userTokenVal);
-    this._freeList.updateAddLikeFreeListing(listId,userTokenVal).subscribe(
-      res=>{
-        setTimeout(() => {
-          /** spinner ends after 5 seconds */
-          this.spinner.hide();
-        }, 1000);
-        this.status = res;
-        console.log(this.status);
-        if (this.status == true) {
-          this._toast.success({ detail: "SUCCESS", summary: 'You liked this listing', position: 'br' });
-          setTimeout(function () {
-            window.location.reload();
-          }, 2000);
-
-        }
-        else {
-          this._toast.warning({ detail: "FAILED", summary: 'Unable to like this listing', position: 'br' });
-
-        }
-      },err=>{
-        setTimeout(() => {
-          /** spinner ends after 5 seconds */
-          this.spinner.hide();
-        }, 1000);
-        this.errMsg = err;
-        this._toast.warning({ detail: "FAILED", summary: 'Please try after sometime', position: 'br' });
-
-      },
-      () => console.log("LIKE LISTING FREE successfully EXECUTED")
-    )
+    
   }
 
 }
