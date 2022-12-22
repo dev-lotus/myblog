ecc3f4f5be40307de6516f4a291115607c2e992e
dev-lotus <61588021+clayer-lotus@users.noreply.github.com>
2022-12-22 23:59:37 +0530

    mongodb url added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors'); 
 const bodyParser = require('body-parser')
-const url = 'mongodb://localhost:27017/shareAndCare'
+const url = 'mongodb+srv://lotus:LGXXK274C2Nxq2OA@cluster0.lodfglu.mongodb.net/test'
 
 // Initliaze express server 
 const app = express();app.use(cors());
@@ -17,11 +17,11 @@ con.on('open', () => {
     console.log('connected');
 })
 
-// app.use('/', (req,res)=>{
-//     res.json({
-//      "status": "Live working fine !"
-//     });
-//  });
+app.use('/', (req,res)=>{
+    res.json({
+     "status": "Live working fine !"
+    });
+ });
 
 // Router 
 const userRouter = require('./routes/user');
